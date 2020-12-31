ab2315081c5ada7c0ce01de43aab67456d9df65b
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-31 19:41:43 +0100

    search form field

--- src/component/form/SearchForm.js
+++ src/component/form/SearchForm.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 function SearchForm() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <form className="form">
+        <label htmlFor="query" className="label">
+          {" "}
+          Search
+        </label>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder={`Search for Paris or Canada`}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SearchForm
+export default SearchForm;
